025c9b63d4a320233f52640f346114786d2ebb21
Mingfai Ma <mingfai.ma@gmail.com>
2008-11-27 10:52:29 +0000

    copied the 20081127 snapshot to a YUI3 branch

---
+++ trunk/build/makeZip.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ../..
+zip -vr inputex-0.2.0.zip inputex-0.2.0 -x "*.svn*" "*.DS_Store*" "*logo-inputex.xcf" "*build.sh" "*makeZip.sh" "*generateDoc.sh" "*inputExRdf*" "*/doc/examples.html" "*_content.html" "*inputex.conf" "*build/tools*"
