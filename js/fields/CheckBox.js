7ab90f6f54b76db64ba530151ecebea40ba9dab6
Mingfai Ma <mingfai.ma@gmail.com>
2008-12-05 01:10:51 +0000

    added IE patch

--- js/fields/CheckBox.js
+++ js/fields/CheckBox.js
@@ -159,9 +159,24 @@
                     this._inputWrapperEl.appendChild(this._hiddenInputEl);
                 }
             },
-
-            _initEvent:function() {
+            
+            /**
+             * @description If IE, add a dirty patch
+             */
+            _initEvents:function() {
                 CheckBox.superclass._initEvents.apply(this, arguments);
+
+                // Awful Hack to work in IE6 and below (the checkbox doesn't fire the change event)
+                // It seems IE 8 removed this behavior from IE7 so it only works with IE 7 ??
+                /*if( YAHOO.env.ua.ie && parseInt(YAHOO.env.ua.ie,10) != 7 ) {
+                 Event.addListener(this.el, "click", function() { this.fireUpdatedEvt(); }, this, true);
+                 }*/
+                if (Y.UA.ie) {
+                    this._inputEl.on('click', Y.bind(function() {
+                        Y.later(10, this, this._inputElOnChange)
+                    }, this))
+                }
+
             },
             /**
              * Method for synchronizing 'value' attribute to the inputEl
@@ -188,82 +203,3 @@
 
     }, '3.0.0pr1', {requires:['field']});
 })();
-
-
-/**
- * Clear the previous events and listen for the "change" event
- */
-/*
- initEvents: function() {
- Event.addListener(this.el, "change", this.onChange, this, true);
-
- // Awful Hack to work in IE6 and below (the checkbox doesn't fire the change event)
- // It seems IE 8 removed this behavior from IE7 so it only works with IE 7 ??
- */
-/*if( YAHOO.env.ua.ie && parseInt(YAHOO.env.ua.ie,10) != 7 ) {
- Event.addListener(this.el, "click", function() { this.fireUpdatedEvt(); }, this, true);
- }*/
-/*
- if( YAHOO.env.ua.ie ) {
- Event.addListener(this.el, "click", function() { YAHOO.lang.later(10,this,this.fireUpdatedEvt); }, this, true);
- }
-
- Event.addFocusListener(this.el, this.onFocus, this, true);
- Event.addBlurListener(this.el, this.onBlur, this, true);
- },
-
- */
-/**
- * Function called when the checkbox is toggled
- * @param {Event} e The original 'change' event
- */
-/*
- onChange: function(e) {
- this.hiddenEl.value = this.el.checked ? this.checkedValue : this.uncheckedValue;
-
- // In IE the fireUpdatedEvent is sent by the click ! We need to send it only once !
- if( !YAHOO.env.ua.ie ) {
- inputEx.CheckBox.superclass.onChange.call(this,e);
- }
- },
-
- */
-/**
- * Get the state value
- * @return {Any} one of [checkedValue,uncheckedValue]
- */
-/*
- getValue: function() {
- return this.el.checked ? this.checkedValue : this.uncheckedValue;
- },
-
- */
-/**
- * Set the value of the checkedbox
- * @param {Any} value The value schould be one of [checkedValue,uncheckedValue]
- * @param {boolean} [sendUpdatedEvt] (optional) Wether this setValue should fire the updatedEvt or not (default is true, pass false to NOT send the event)
- */
-/*
- setValue: function(value, sendUpdatedEvt) {
- if (value===this.checkedValue) {
- this.hiddenEl.value = value;
- this.el.checked = true;
- }
- else {
- // DEBUG :
- */
-/*if (value!==this.uncheckedValue && lang.isObject(console) && lang.isFunction(console.log) ) {
- console.log("inputEx.CheckBox: value is *"+value+"*, schould be in ["+this.checkedValue+","+this.uncheckedValue+"]");
- }*/
-/*
- this.hiddenEl.value = value;
- this.el.checked = false;
- }
-
- // Call Field.setValue to set class and fire updated event
- inputEx.CheckBox.superclass.setValue.call(this,value, sendUpdatedEvt);
- }
-
- });
-
- */
